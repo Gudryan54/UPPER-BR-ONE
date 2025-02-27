596d24014b84bcc31852a21361c71fb558845b04
Gustavo Gudryan <gustavociric98@gmail.com>
2025-02-27 09:49:26 -0300

    Adicionando pasta _static e favicon uppertools.ico

--- Docs/html/conf.py
+++ Docs/html/conf.py
@@ -67,7 +67,8 @@ html_css_files = [
 html_show_sourcelink = False
 
 # Inclusão do favicon
-html_favicon = '/var/www/html/img_logo/logo_grande/uppertools.ico'
+html_favicon = '_static/uppertools.ico'
+
 
 
 # Diretório para arquivos estáticos
